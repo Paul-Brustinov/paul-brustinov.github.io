61f5b1e03164459b24880f0188754b52811c230a
Paul-Brustinov <b.paul4@gmail.com>
2022-06-06 20:40:35 +0300

    Update Find_Column.sql

--- SQL/Find_Column.sql
+++ SQL/Find_Column.sql
@@ -1,1 +1,12 @@
-declare @p nvarchar(120) = N'%Agent%'select SCHEMA_NAME(o.schema_id) + '.' + OBJECT_NAME(o.object_id) --*from sys.objects owhere OBJECT_DEFINITION(o.object_id) like @pselect SCHEMA_NAME(o.schema_id) + '.' + OBJECT_NAME(o.object_id), o.type, c.*from sys.columns c	inner join sys.objects o on o.object_id = c.object_idwhere c.name like @p	and o.type <> 'V'order by o.name
+declare @p nvarchar(120) = N'%Agent%'
+
+select SCHEMA_NAME(o.schema_id) + '.' + OBJECT_NAME(o.object_id) --*
+from sys.objects o
+where OBJECT_DEFINITION(o.object_id) like @p
+
+select SCHEMA_NAME(o.schema_id) + '.' + OBJECT_NAME(o.object_id), o.type, c.*
+from sys.columns c
+	inner join sys.objects o on o.object_id = c.object_id
+where c.name like @p
+  and o.type <> 'V'
+order by o.name
